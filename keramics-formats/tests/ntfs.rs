f52f308c09bbd97f7f930825c98e0ade09d9f6a3
Joachim Metz <joachim.metz@gmail.com>
2025-11-16 07:33:05 +0100

    Worked on virtual file system (VFS) and code clean up

--- keramics-formats/tests/ntfs.rs
+++ keramics-formats/tests/ntfs.rs
@@ -15,7 +15,8 @@ use std::path::PathBuf;
 
 use keramics_core::formatters::format_as_string;
 use keramics_core::{DataStreamReference, ErrorTrace, open_os_data_stream};
-use keramics_formats::ntfs::{NtfsFileEntry, NtfsFileSystem, NtfsPath};
+use keramics_formats::Path;
+use keramics_formats::ntfs::{NtfsFileEntry, NtfsFileSystem};
 use keramics_hashes::{DigestHashContext, Md5Context};
 
 fn read_data_stream(data_stream: &DataStreamReference) -> Result<(u64, String), ErrorTrace> {
@@ -46,14 +47,14 @@ fn read_data_stream(data_stream: &DataStreamReference) -> Result<(u64, String), 
     Ok((offset, hash_string))
 }
 
-fn read_path(file_system: &NtfsFileSystem, path: &str) -> Result<(u64, String), ErrorTrace> {
-    let path: NtfsPath = NtfsPath::from(path);
+fn read_path(file_system: &NtfsFileSystem, path_string: &str) -> Result<(u64, String), ErrorTrace> {
+    let path: Path = Path::from(path_string);
     let result: Option<NtfsFileEntry> = match file_system.get_file_entry_by_path(&path) {
         Ok(result) => result,
         Err(mut error) => {
             keramics_core::error_trace_add_frame!(
                 error,
-                format!("Unable to retrieve file entry: {}", path.to_string())
+                format!("Unable to retrieve file entry: {}", path_string)
             );
             return Err(error);
         }
@@ -62,8 +63,8 @@ fn read_path(file_system: &NtfsFileSystem, path: &str) -> Result<(u64, String), 
         Some(file_entry) => file_entry,
         None => {
             return Err(keramics_core::error_trace_new!(format!(
-                "Missing file entry: {}",
-                path.to_string()
+                "Missing data stream for file entry: {}",
+                path_string
             )));
         }
     };
@@ -102,7 +103,7 @@ fn read_ntfs_empty_file() -> Result<(), ErrorTrace> {
     let path_buf: PathBuf = PathBuf::from("../test_data/ntfs/ntfs.raw");
     let file_system: NtfsFileSystem = open_file_system(&path_buf)?;
 
-    let (offset, md5_hash): (u64, String) = read_path(&file_system, "\\emptyfile")?;
+    let (offset, md5_hash): (u64, String) = read_path(&file_system, "/emptyfile")?;
     assert_eq!(offset, 0);
     assert_eq!(md5_hash.as_str(), "d41d8cd98f00b204e9800998ecf8427e");
 
